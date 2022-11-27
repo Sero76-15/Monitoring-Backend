91d23e9c3296cab4d0ef446b9b29b485ee14095b
Serhat Cayukli <c.semetserhat@gmail.com>
2022-11-27 23:24:31 +0100

    removed possibility to save the same urls

--- src/main/java/com/sero76/monitoringbackend/services/ServerService.java
+++ src/main/java/com/sero76/monitoringbackend/services/ServerService.java
@@ -2,6 +2,7 @@ package com.sero76.monitoringbackend.services;
 
 import com.sero76.monitoringbackend.models.Server;
 import com.sero76.monitoringbackend.repositories.ServerRepository;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -21,36 +22,40 @@ public class ServerService {
 
     /**
      * Saving the given server in the database
-     * */
-    public void saveServer(Server server) {
-        this.repository.save(server);
-    }
-
-    /**
-     * get server by id
-     * */
-    public Server getServerById(long id) {
-        return this.repository.findById(id).orElseThrow(() -> {
-            throw new EntityNotFoundException("Server with id [" + id + "] not found");
-        });
-    }
-
-    /**
-     * get all servers saved in the database
-     * */
-    public List<Server> getAllServers() {
-        List<Server> servers = new ArrayList<>();
-        Iterator<Server> iterator = this.repository.findAll().iterator();
-        while(iterator.hasNext()) {
-            servers.add(iterator.next());
+     */
+    public Server saveServer(Server server) {
+        Server serverExists = this.repository.findByUrl(server.getUrl());
+        if (serverExists == null) {
+            return this.repository.save(server);
         }
-        return servers;
+        throw new EntityExistsException("Server with URL [" + server.getUrl() + "] exists.");
     }
 
-    /**
-     * delete server by id
-     * */
-    public void deleteServer(long id) {
-        this.repository.deleteById(id);
+        /**
+         * get server by id
+         * */
+        public Server getServerById ( long id){
+            return this.repository.findById(id).orElseThrow(() -> {
+                throw new EntityNotFoundException("Server with id [" + id + "] not found");
+            });
+        }
+
+        /**
+         * get all servers saved in the database
+         * */
+        public List<Server> getAllServers () {
+            List<Server> servers = new ArrayList<>();
+            Iterator<Server> iterator = this.repository.findAll().iterator();
+            while (iterator.hasNext()) {
+                servers.add(iterator.next());
+            }
+            return servers;
+        }
+
+        /**
+         * delete server by id
+         * */
+        public void deleteServerById ( long id){
+            this.repository.deleteById(id);
+        }
     }
-}
