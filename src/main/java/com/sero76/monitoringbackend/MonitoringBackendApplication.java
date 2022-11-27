0690a8448d49e16fc5f0c19a30930ae3cbadf6d8
Serhat Cayukli <c.semetserhat@gmail.com>
2022-11-27 18:04:11 +0100

    v1 - spring boot app created

---
+++ src/main/java/com/sero76/monitoringbackend/MonitoringBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sero76.monitoringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringBackendApplication.class, args);
+    }
+}
