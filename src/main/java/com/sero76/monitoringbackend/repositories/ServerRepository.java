8f62dd2ad11cc4be38e57c4e6e0d24052bdf58a9
Serhat Cayukli <c.semetserhat@gmail.com>
2022-11-27 18:53:04 +0100

    created repository

---
+++ src/main/java/com/sero76/monitoringbackend/repositories/ServerRepository.java
@@ -0,0 +1,9 @@
+package com.sero76.monitoringbackend.repositories;
+
+import com.sero76.monitoringbackend.models.Server;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServerRepository extends CrudRepository<Server, Long> {
+}
